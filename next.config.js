4a4605de00c59c2aebf50e768df1b5c761775ead
Heydi <lucasheydi@gmail.com>
2025-03-04 16:35:23 +0900

    feat: atualizações do projeto

--- next.config.js
+++ next.config.js
@@ -40,10 +40,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Re-enabling experimental CSS optimization (make sure critters is installed)
-  experimental: {
-    optimizeCss: true
+  // Completely removing experimental options to avoid the critters error
+  // experimental: {
+  //   optimizeCss: true
+  // }
+  
+  // Add this to catch more detailed module resolution errors
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
   }
-};
-
-module.exports = nextConfig;
+}
